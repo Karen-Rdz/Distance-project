09486efecf588d883737661a57a3de23bc7b8ca1
Karen-Rdz <31635951+Karen-Rdz@users.noreply.github.com>
2021-02-15 17:31:23 -0600

    Added visualization
    Added standarization and some visualization of the best 2 features

--- Wearables Python/Data for Models/SVMs.py
+++ Wearables Python/Data for Models/SVMs.py
@@ -18,16 +18,15 @@ from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
 from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler 
 
 # Get data
 data_complete = scipy.io.loadmat('FeatureMatrices.mat')
 labels = data_complete['FMinfo']
 test = data_complete['WFM_1']
-clas = test[:, 129]
 
 x = np.delete(test, 129, axis=1)
-y = clas
-# print(x)
+y = test[:, 129]
 
 # Variables
 num_features = 10
@@ -48,42 +47,27 @@ for j in range (1, times+1):
         # Las k mejores características
         X_new = SelectKBest(chi2, k=num_features).fit_transform(abs(x), y)
 
-        # Split the data for train and test
-        X_train, X_test, y_train, y_test = train_test_split(X_new, y, test_size=0.3) 
-        #print(X_train)
-
         # Standardization of data
-        scaler = preprocessing.StandardScaler().fit(X_train)
-        X_scaled = scaler.transform(X_train)
+        scaler = StandardScaler().fit(X_new)
+        X_scaled = scaler.transform(X_new)
 
-        # RBF, Polynomial and Linear Kernel
-        # rbf = svm.SVC(kernel='rbf', gamma=0.6, C=1).fit(X_train, y_train)
-        # poly = svm.SVC(kernel='poly', degree=3, C=1).fit(X_train, y_train)
-        # linear = svm.SVC(kernel='linear').fit(X_train, y_train)
+        # Split the data for train and test
+        X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.3) 
 
         # RBF, Polynomial and Linear Kernel with standardization
-        rbf = svm.SVC(kernel='rbf', gamma=0.6, C=1).fit(X_scaled, y_train)
-        poly = svm.SVC(kernel='poly', degree=3, C=1).fit(X_scaled, y_train)
-        linear = svm.SVC(kernel='linear').fit(X_scaled, y_train)
+        rbf = svm.SVC(kernel='rbf', gamma=0.6, C=1).fit(X_train, y_train)
+        poly = svm.SVC(kernel='poly', degree=3, C=1).fit(X_train, y_train)
+        linear = svm.SVC(kernel='linear').fit(X_train, y_train)
 
-    #     poly_pred_test = poly.predict(X_test)
-    #     rbf_pred_test = rbf.predict(X_test)
-    #     linear_pred_test = linear.predict(X_test)
-
+        # Predict values
         poly_pred_test = cross_val_predict(poly, X_test, y_test, cv = 5)
         rbf_pred_test = cross_val_predict(rbf, X_test, y_test, cv = 5)
         linear_pred_test = cross_val_predict(linear, X_test, y_test, cv = 5)
-        #print(linear_pred_test)
 
         # Accuracy test
         poly_accuracy = accuracy_score(y_test, poly_pred_test)
-        poly_f1 = f1_score(y_test, poly_pred_test, average='weighted')
-
         rbf_accuracy = accuracy_score(y_test, rbf_pred_test)
-        rbf_f1 = f1_score(y_test, rbf_pred_test, average='weighted')
-
         linear_accuracy = accuracy_score(y_test, linear_pred_test)
-        linear_f1 = f1_score(y_test, linear_pred_test, average='weighted')
 
         if poly_accuracy > rbf_accuracy and poly_accuracy > linear_accuracy and poly_accuracy > best_accuracy:
             best_accuracy = poly_accuracy
@@ -97,11 +81,66 @@ for j in range (1, times+1):
             best_accuracy = linear_accuracy
             best_kernel = 'Linear'
             best_features = i
-    dict_features[best_features] = dict_features[best_features] + 1
+    dict_features[best_features] = dict_features[best_features] + 1 
     print(dict_features)
         
-# print('Best Accuracy: ', "%.2f" % (best_accuracy*100))
-# print('Best Kernel: ', best_kernel)
-# print('Number of features: ', best_features)
-print('----- Final Result -----')
+    print('Best Accuracy: ', "%.2f" % (best_accuracy*100))
+    print('Best Kernel: ', best_kernel)
+    print('Number of features: ', best_features)
+print('----- Final results -----')
 print(dict_features)
+
+# 2 best features
+X_new = SelectKBest(chi2, k=2).fit_transform(abs(x), y)
+best_features = []
+count = 0
+
+# Know which are the best features
+for i in X_new[0]:
+    for j in abs(x[0]):
+        count += 1
+        if i == j:
+            best_features.append(count)
+            count = 0
+            break
+print(best_features)
+print('--------')            
+
+# Standardization of data
+scaler = preprocessing.StandardScaler().fit(X_new)
+X_scaled = scaler.transform(X_new)
+
+# Plot data
+plt.plot(X_new, "ob")
+plt.title('Before Standardization')
+plt.show()
+
+plt.plot(X_scaled, "ob")
+plt.title('After Standardization')
+plt.show()
+
+# Plot 2D
+col = []
+for i in range(0, len(y)):
+    if y[i] == 1:
+        col.append('g')
+    elif y[i] == 2:
+        col.append('y')
+    elif y[i] == 3:
+        col.append('r')
+    plt.plot(X_scaled[i, 1], X_scaled[i, 0], col[i]+"o")
+plt.title('2 features 2D')
+plt.xlabel('X Scaled 129')
+plt.ylabel('X Scaled 33')
+plt.show()
+
+# Plot 3D
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+for i in range(0, len(y)):
+    ax.scatter3D(X_scaled[i, 1], X_scaled[i, 0], y[i], c=col[i], marker = 'o')
+plt.title('2 features 3D')
+ax.set_xlabel('X Scaled 129')
+ax.set_ylabel('X Scaled 33')
+ax.set_zlabel('Y clas')
+plt.show()
